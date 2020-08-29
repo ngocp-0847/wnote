6c10ab0893b1c4b96560fdda619ae8a94594930e
phan.ngoc <phan.ngoc@sun-asterisk.com>
2020-08-29 18:38:06 +0700

    [Add] deployer

---
+++ deploy.php
@@ -0,0 +1,66 @@
+<?php
+namespace Deployer;
+require 'recipe/common.php';
+
+// Project name
+set('application', 'w-note');
+
+// Project repository
+set('repository', 'git@github.com:ngocp-0847/wnote.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys
+add('shared_files', ['.env']);
+add('shared_dirs', [
+    'public/upload',
+]);
+
+set('writable_use_sudo', true);
+set('allow_anonymous_stats', false);
+set('cleanup_use_sudo', true);
+
+set('branch', 'master');
+
+// Hosts
+host('52.15.199.9')
+    ->stage('production')
+    ->roles('app')
+    ->user('ec2-user')
+    ->set('deploy_path', '/var/www/html/{{application}}');
+
+task('npm:install', function () {
+    run('cd {{release_path}} && yarn');
+});
+
+task('npm:run:build', function () {
+    run('cd {{release_path}} && yarn build');
+});
+
+task('pm2:init', function () {
+    run('cd {{current_path}} && pm2 start yarn --interpreter bash --name nextjs -- start');
+});
+
+task('pm2:restart', function () {
+    run('cd {{current_path}} && pm2 restart nextjs');
+});
+
+desc('Deploy your project');
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'npm:install',
+    'npm:run:build',
+    'deploy:writable',
+    'deploy:symlink',
+    'deploy:unlock',
+    'pm2:restart',
+    'cleanup',
+]);
+
+desc('Deployed successfully!');
